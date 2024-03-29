2356ca1e7c2fe808a492a6f481451a6723eabd2b
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-03-29 23:38:35 +0900

    refactor BubbleSort

--- src/com/jjh/sorting/BubbleSort.java
+++ src/com/jjh/sorting/BubbleSort.java
@@ -3,21 +3,22 @@ package com.jjh.sorting;
 public class BubbleSort {
     public void sort(int[] numbers) {
         int size = numbers.length;
-        if (size == 0) {
-            return;
-        }
-
+        boolean sorted;
         for (int i = 0; i < size; i++) {
-            boolean sorted = false;
+            sorted = true;
             for (int j = 1; j < size -i; j++) {
                 if (numbers[j] < numbers[j-1]) {
-                    sorted = true;
-                    int tmp = numbers[j];
-                    numbers[j] = numbers[j-1];
-                    numbers[j-1] = tmp;
+                    sorted = false;
+                    swap(numbers, j, j-1);
                 }
             }
-            if (!sorted) return;
+            if (sorted) return;
         }
+    }
+
+    private void swap(int[] numbers, int index1, int index2) {
+        int tmp = numbers[index1];
+        numbers[index1] = numbers[index2];
+        numbers[index2] = tmp;
     }
 }
