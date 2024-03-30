cd229f3278eadb85a09a463e907351414dd3000e
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-03-30 10:54:41 +0900

    refactor SelectionSort

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         SelectionSort selectionSort = new SelectionSort();
-        int[] arr = new int[]{4,2,2,6,4,3,2};
-        selectionSort.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr1 = new int[]{4,2,2,6,4,3,2};
+        int[] arr2 = new int[]{7,3};
+        int[] arr3 = new int[]{7};
+        int[] arr4 = new int[]{};
+        selectionSort.sort(arr4);
+        System.out.println(Arrays.toString(arr4));
     }
 }
--- src/com/jjh/sorting/SelectionSort.java
+++ src/com/jjh/sorting/SelectionSort.java
@@ -3,18 +3,25 @@ package com.jjh.sorting;
 public class SelectionSort {
     public void sort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            int min = arr[i];
-            int minIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (min > arr[j]) {
-                 min = arr[j];
-                 minIndex = j;
-                }
-            }
-            // store minimum value in temp variable
-            int tmp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = tmp;
+            int minIndex = findMinIndex(arr, i);
+            swap(arr, minIndex, i);
         }
+    }
+
+    private int findMinIndex(int[] arr, int i) {
+        int minIndex = i;
+        for (int j = i; j < arr.length; j++) {
+            if (arr[minIndex] > arr[j]) {
+             minIndex = j;
+            }
+        }
+        return minIndex;
+    }
+
+    private void swap(int[] arr, int index1, int index2) {
+        // store minimum value in temp variable
+        int tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
     }
 }
