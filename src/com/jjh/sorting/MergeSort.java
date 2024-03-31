8bfe0d9ed057a147946b48d7d615c1f2c70c150c
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-03-31 12:47:48 +0900

    refactor MergeSort

--- src/com/jjh/sorting/MergeSort.java
+++ src/com/jjh/sorting/MergeSort.java
@@ -4,32 +4,33 @@ import java.util.Arrays;
 
 public class MergeSort {
     public int[] sort(int[] arr) {
+        // base condition: when to stop recursion?
+        // when there's only one single item in the array.
         if (arr.length > 1) {
             // get the min index
             int mid = arr.length / 2;
             int[] left =  sort(Arrays.copyOfRange(arr, 0, mid));
             int[] right = sort(Arrays.copyOfRange(arr, mid, arr.length));
+            // merge the result
             merge(arr, left, right);
         }
         return arr;
     }
 
-    private void merge(int[] arr, int[] left, int[] right) {
-        int i = 0;
-        int j = 0;
-        int k = 0;
+    private void merge(int[] result, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
         while (i < left.length && j < right.length) {
             if (left[i] <= right[j]) {
-                arr[k++] = left[i++];
+                result[k++] = left[i++];
             } else {
-                arr[k++] = right[j++];
+                result[k++] = right[j++];
             }
         }
         while (i < left.length) {
-            arr[k++] = left[i++];
+            result[k++] = left[i++];
         }
         while (j < right.length) {
-            arr[k++] = right[j++];
+            result[k++] = right[j++];
         }
     }
 }
