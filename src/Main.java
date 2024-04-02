1e875bac3ff8546c2ca8cfd12bd963382fe1b3be
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-04-02 23:48:14 +0900

    refactor binary search

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Search search = new Search();
         int[] arr = new int[] {1,2,3,4,5,6,7,8};
-        int index1 = search.binarySearch_iterative(arr, 2);
+        int index1 = search.binarySearch_iterative(arr, 102);
         System.out.println("index1: "  + index1);
     }
 }
--- src/com/jjh/searching/Search.java
+++ src/com/jjh/searching/Search.java
@@ -9,38 +9,38 @@ public class Search {
         }
         return -1;
     }
+    public int binarySearch_recursive(int[] arr, int key) {
+        return binarySearch_recursive(arr, key, 0, arr.length - 1);
+    }
 
-    public int binarySearch_recursive(int[] arr, int key, int left, int right) {
-        if (left > right) {
-            return -1;
-        }
+    private int binarySearch_recursive(int[] arr, int key, int left, int right) {
+        // base condition: out partition or segment is empty
+        if (left > right) return -1;
+
         int mid = (right + left) / 2;
-        if (arr[mid] == key) {
-            return mid;
-        }
-        int foundValue;
-        if (arr[mid] < key) {
-            foundValue = binarySearch_recursive(arr, key, mid + 1, right);
-        } else {
-            foundValue = binarySearch_recursive(arr, key, left, mid -1);
-        }
-        return foundValue;
+
+        if (arr[mid] == key) return mid;
+
+        if (arr[mid] < key)
+            return binarySearch_recursive(arr, key, mid + 1, right);
+
+        return binarySearch_recursive(arr, key, left, mid -1);
     }
 
     public int binarySearch_iterative(int[] arr, int key) {
         int left = 0;
         int right = arr.length - 1;
-
+        // While we have a partition or a segment that has at least one element.
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (arr[mid] == key) {
+
+            if (arr[mid] == key)
                 return mid;
-            }
-            if (key < arr[mid]) {
+
+            if (key < arr[mid])
                 right = mid -1;
-            } else {
+            else
                 left = mid + 1;
-            }
         }
         return -1;
     }
