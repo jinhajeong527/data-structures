68aa796ca0209252d7cb6b14b04efac4e5579e0d
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-04-02 23:02:58 +0900

    implement LinearSearch

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
+import com.jjh.searching.LinearSearch;
 import com.jjh.sorting.*;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BucketSort bucketSort = new BucketSort();
-        int[] arr1 = new int[]{4,3,2,4,2,9,5,1,7};
-        int[] arr2 = new int[]{};
-        int[] arr3 = new int[]{1,2,2,0};
-        int[] arr4 = new int[]{1};
-        bucketSort.sort(arr1, 4);
-        System.out.println(Arrays.toString(arr1));
+        LinearSearch linearSearch = new LinearSearch();
+        int[] arr = new int[] {1,5,3,7,8};
+        int index1 = linearSearch.search(arr, 7);
+        int index2 = linearSearch.search(arr, 6);
+        System.out.println("index1:"  + index1);
+        System.out.println("index2:" + index2);
     }
 }
---
+++ src/com/jjh/searching/LinearSearch.java
@@ -0,0 +1,12 @@
+package com.jjh.searching;
+
+public class LinearSearch {
+    public int search(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
