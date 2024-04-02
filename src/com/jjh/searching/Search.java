b9e3b8038244186ba754b43495828e6be19a7caa
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-04-02 23:33:10 +0900

    implement binarysearch iterative way

--- src/com/jjh/searching/Search.java
+++ src/com/jjh/searching/Search.java
@@ -26,4 +26,22 @@ public class Search {
         }
         return foundValue;
     }
+
+    public int binarySearch_iterative(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            }
+            if (key < arr[mid]) {
+                right = mid -1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
