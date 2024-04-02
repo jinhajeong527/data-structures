061d8bb5a09a4f0d7a3246cf15180caca2dd54dc
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-04-02 23:26:01 +0900

    implement binarysearch recursive way

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@ import com.jjh.searching.Search;
 public class Main {
     public static void main(String[] args) {
         Search search = new Search();
-        int[] arr = new int[] {1,5,3,7,8};
-        int index1 = search.linearSearch(arr, 7);
-        int index2 = search.linearSearch(arr, 6);
+        int[] arr = new int[] {1,2,3,4,5,6,7,8};
+        int index1 = search.binarySearch_recursive(arr, 8, 0, arr.length-1);
         System.out.println("index1: "  + index1);
-        System.out.println("index2: " + index2);
     }
 }
--- src/com/jjh/searching/Search.java
+++ src/com/jjh/searching/Search.java
@@ -9,4 +9,21 @@ public class Search {
         }
         return -1;
     }
+
+    public int binarySearch_recursive(int[] arr, int key, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = (right + left) / 2;
+        if (arr[mid] == key) {
+            return mid;
+        }
+        int foundValue;
+        if (arr[mid] < key) {
+            foundValue = binarySearch_recursive(arr, key, mid + 1, right);
+        } else {
+            foundValue = binarySearch_recursive(arr, key, left, mid -1);
+        }
+        return foundValue;
+    }
 }
