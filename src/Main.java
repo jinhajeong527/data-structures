0cc6434c810979690e86ed1a4df9fefe5e9be03e
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-04-19 14:30:20 +0900

    implement finding the shortest distance using dijkstra algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import com.jjh.graphs.Graph;
 import com.jjh.graphs.WeightedGraph;
 
 public class Main {
@@ -7,8 +6,15 @@ public class Main {
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
+        graph.addNode("D");
+        graph.addNode("E");
         graph.addEdge("A", "B", 3);
         graph.addEdge("A", "C", 2);
-        graph.print();
+        graph.addEdge("A", "D", 4);
+        graph.addEdge("C", "D", 4);
+        graph.addEdge("B", "D", 5);
+        graph.addEdge("B", "E", 5);
+        graph.addEdge("D", "E", 6);
+        System.out.println(graph.getShortestDistance("A", "E"));
     }
 }
--- src/com/jjh/graphs/WeightedGraph.java
+++ src/com/jjh/graphs/WeightedGraph.java
@@ -67,4 +67,53 @@ public class WeightedGraph {
             }
         }
     }
+
+    private class NodeEntry {
+        private Node node;
+        private int priority;
+
+        public NodeEntry(Node node, int priority) {
+            this.node = node;
+            this.priority = priority;
+        }
+    }
+
+
+    public int getShortestDistance(String from, String to) {
+        Map<Node, Integer> distances = new HashMap<>();
+
+        for (Node node: nodes.values()) {
+            distances.put(node, Integer.MAX_VALUE);
+        }
+
+        Node fromNode = nodes.get(from);
+        if (fromNode == null) {
+            throw new IllegalArgumentException();
+        }
+        // Replace distance of starting Node from Integer.MAX_VALUE to 0
+        distances.replace(fromNode, 0);
+
+        Set<Node> visited = new HashSet<>();
+
+        PriorityQueue<NodeEntry> queue = new PriorityQueue<>(
+                Comparator.comparingInt(ne -> ne.priority));
+        queue.add(new NodeEntry(fromNode, 0));
+
+        while (!queue.isEmpty()) {
+            Node current = queue.remove().node;
+            visited.add(current);
+
+            for (Edge edge: current.getEdges()) {
+                if (visited.contains(edge.to))
+                    continue;
+                int distance = distances.get(edge.to);
+                int newDistance = distances.get(current) + edge.weight;
+                if (distance > newDistance) {
+                    distances.replace(edge.to, newDistance);
+                    queue.add(new NodeEntry(edge.to, newDistance));
+                }
+            }
+        }
+        return distances.get(nodes.get(to));
+    }
 }
