e27ac03bad652e1fefd1519cb2cb8a8895dade7e
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-04-22 17:00:46 +0900

    implement Tree insert using recursion

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,11 @@
-import com.jjh.graphs.WeightedGraph;
+import com.jjh.avltrees.AVLTree;
 
 public class Main {
     public static void main(String[] args) {
-        WeightedGraph graph = new WeightedGraph();
-        graph.addNode("A");
-        graph.addNode("B");
-        graph.addNode("C");
-        graph.addNode("D");
-        graph.addEdge("A", "B", 4);
-        graph.addEdge("A", "C", 2);
-        graph.addEdge("A", "D", 6);
-        graph.addEdge("B", "C", 7);
-        graph.addEdge("B", "D", 5);
-        graph.addEdge("C", "D", 5);
-        WeightedGraph tree = graph.getMinimumSpanningTree();
-        tree.print();
+        AVLTree tree = new AVLTree();
+        tree.insert(3);
+        tree.insert(5);
+        tree.insert(7);
 
     }
 }
---
+++ src/com/jjh/avltrees/AVLTree.java
@@ -0,0 +1,35 @@
+package com.jjh.avltrees;
+
+public class AVLTree {
+    private AVLNode root;
+    private class AVLNode {
+        private int value;
+        private AVLNode leftChild;
+        private AVLNode rightChild;
+
+        public AVLNode(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Value=" + this.value;
+        }
+    }
+
+    public void insert(int value) {
+        root = insert(root, value);
+    }
+
+    private AVLNode insert(AVLNode root, int value) {
+        if (root == null) {
+            return new AVLNode(value);
+        }
+        if (value < root.value)
+            root.leftChild = insert(root.leftChild, value);
+        else
+            root.rightChild = insert(root.rightChild, value);
+
+        return root;
+    }
+}
