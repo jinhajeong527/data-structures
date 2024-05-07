51592208f2560dd93465ec852e381c8a99b79e17
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-05-07 20:39:01 +0900

    refactor : change public Node class to private in MinHeap

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,12 @@ import com.jjh.heaps.MinHeap;
 
 public class Main {
     public static void main(String[] args) {
-        MinHeap minHeap = new MinHeap();
-        minHeap.insert(10, "a");
-//        minHeap.insert(9, "b");
-//        minHeap.insert(8, "c");
-//        minHeap.insert(6, "d");
-//        minHeap.insert(3, "e");
-        String value = minHeap.remove();
-//        String value2 = minHeap.remove();
-//        String value3 = minHeap.remove();
-//        String value4 = minHeap.remove();
-//        String value5 = minHeap.remove();
-        System.out.println(value);
-//        System.out.println(value2);
-//        System.out.println(value3);
-//        System.out.println(value4);
-//        System.out.println(value5);
+        MinHeap mh = new MinHeap();
+        mh.insert(7, "a");
+        mh.insert(5, "b");
+        mh.insert(3, "c");
+        mh.insert(1, "d");
+        mh.remove();
+        System.out.println("Done");
     }
 }
--- src/com/jjh/heaps/MinHeap.java
+++ src/com/jjh/heaps/MinHeap.java
@@ -1,18 +1,25 @@
 package com.jjh.heaps;
 
-import java.util.Collections;
-import java.util.List;
-
 public class MinHeap {
-
-    private final Node[] heap = new Node[10];
+    private Node[] nodes = new Node[10];
     private int size;
+
+    private class Node {
+        int key;
+        String value;
+
+        public Node(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
     public void insert(int key, String value) {
         if (isFull()) {
             throw new IllegalStateException();
         }
-        heap[size++] = new Node(key, value);
+        Node node = new Node(key, value);
+        nodes[size++] = node;
         bubbleUp();
     }
 
@@ -20,119 +27,93 @@ public class MinHeap {
         if (isEmpty()) {
             throw new IllegalStateException();
         }
-        Node root = heap[0];
-        heap[0] = heap[--size];
-
+        Node node = nodes[0];
+        nodes[0] = nodes[--size];
         bubbleDown();
-        return root.getValue();
+        return node.value;
     }
 
-    public boolean isFull() {
-        return size == heap.length;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    private void bubbleUp() {
-        // When insert() method called, size increased by one.
-        // So size minus 1 is the last index of this array
+    public void bubbleUp() {
         int index = size - 1;
-        while (index > 0 && heap[parent(index)].getKey() > heap[index].getKey()) {
-            swap(heap, parent(index), index);
+        while (index > 0 && nodes[parent(index)].key > nodes[index].key) {
+            swap(parent(index), index);
             index = parent(index);
         }
     }
 
-    private void bubbleDown() {
+    public void bubbleDown() {
         int index = 0;
         while (index < size && !isValidParent(index)) {
-            int smallerChild = smallerChildIndex(index);
-            swap(heap, index, smallerChild);
-            index = smallerChild;
+            int smallerIndex = smallerChildIndex(index);
+            swap(index, smallerIndex);
+            index = smallerIndex;
         }
+    }
+
+    public int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private boolean hasLeftChild(int index) {
+        return leftChildIndex(index) < size;
+    }
+
+    private boolean hasRightChild(int index) {
+        return rightChildIndex(index) < size;
     }
 
     private boolean isValidParent(int index) {
         if (!hasLeftChild(index)) {
             return true;
         }
-        boolean isValid = heap[index].getKey() <= heap[leftChild(index)].getKey();
+        boolean isValid = nodes[index].key < leftChild(index).key;
 
         if (hasRightChild(index)) {
-            isValid &= heap[index].getKey() <= heap[rightChild(index)].getKey();
+            isValid &= nodes[index].key < rightChild(index).key;
         }
+
         return isValid;
-    }
-
-    private boolean hasLeftChild(int index) {
-        return leftChild(index) < size;
-    }
-
-    private boolean hasRightChild(int index) {
-        return rightChild(index) < size;
     }
 
     private int smallerChildIndex(int index) {
         if (!hasLeftChild(index)) {
             return index;
         }
-
         if (!hasRightChild(index)) {
-            return leftChild(index);
+            return leftChildIndex(index);
         }
-
-        return heap[leftChild(index)].getKey() < heap[rightChild(index)].getKey() ?
-                leftChild(index) : rightChild(index);
-
+        return leftChild(index).key > rightChild(index).key ?
+                rightChildIndex(index) : leftChildIndex(index);
     }
 
-
-    private int parent(int index) {
-        return (index - 1) / 2;
+    private Node leftChild(int index) {
+        return nodes[leftChildIndex(index)];
     }
 
-    private int leftChild(int index) {
+    private int leftChildIndex(int index) {
         return index * 2 + 1;
     }
 
-    private int rightChild(int index) {
+    private Node rightChild(int index) {
+        return nodes[rightChildIndex(index)];
+    }
+
+    private int rightChildIndex(int index) {
         return index * 2 + 2;
-     }
-
-    private void swap(Node[] heap, int first, int second) {
-        Node temp = heap[first];
-        heap[first] = heap[second];
-        heap[second] = temp;
     }
 
-
-    public static void heapify(List<Node> list) {
-        int lastParentIndex = list.size() / 2 - 1;
-        for (int i = lastParentIndex; i >= 0; i--) {
-            heapify(list, i);
-        }
+    private boolean isFull() {
+        return size == nodes.length;
     }
 
-    private static void heapify(List<Node> list, int index) {
-
-        int smallerIndex = index;
-
-        int left = index * 2 + 1;
-        if (left < list.size() && list.get(smallerIndex).getKey() > list.get(left).getKey()) {
-            smallerIndex = left;
-        }
-
-        int right = index * 2 + 2;
-        if (right < list.size() && list.get(smallerIndex).getKey() > list.get(right).getKey()) {
-            smallerIndex = right;
-        }
-
-        if (index == smallerIndex) {
-            return;
-        }
-        Collections.swap(list, index, smallerIndex);
-        heapify(list, smallerIndex);
+    private boolean isEmpty() {
+        return size == 0;
     }
+
+    private void swap(int first, int second) {
+        Node temp = nodes[first];
+        nodes[first] = nodes[second];
+        nodes[second] = temp;
+    }
+
 }
